ef6fa74c4b4cb2bc29b9232d9fe09c656efb91ae
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-10-31 18:05:16 +0530

    "ads2_m3_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2_Assignments/m3/3.1 Directed Cycle/Solution.java
+++ ADS-2_Assignments/m3/3.1 Directed Cycle/Solution.java
@@ -7,21 +7,18 @@ class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int v = Integer.parseInt(sc.nextLine());
-		Bag bag = new Bag();
-		Digraph g = new Digraph(v);
-        DirectedCycle finder = new DirectedCycle(g);
-	    
-	    /*for (int i = 0; i < v; i++) {
-        	adj[v] = new Bag<Integer>();
-    	}*/
     	int E = Integer.parseInt(sc.nextLine());
-        if (E < 0) throw new IllegalArgumentException("number of edges in a Digraph must be nonnegative");
+		
+		Digraph g = new Digraph(v);
+	    
+        
         while (sc.hasNextLine()) {
             String[] tokens = sc.nextLine().split(" ");
             int a = Integer.parseInt(tokens[0]);
             int w = Integer.parseInt(tokens[1]);
             g.addEdge(a, w); 
         }
+        DirectedCycle finder = new DirectedCycle(g);
         if (finder.hasCycle()) {
             System.out.println("Cycle exists.");
         } else {
