4a8212192031bfd98388bf9375bc2aea3938fca7
Arepallyrakesh <rk.rakhi514@msitprogram.net>
2018-11-12 16:58:05 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2_Assignments/m13/Radix Sorts/Solution.java
+++ ADS-2_Assignments/m13/Radix Sorts/Solution.java
@@ -4,31 +4,31 @@ import java.util.Arrays;
 /**
  * Class for solution.
  */
-class Solution {
+public final class Solution {
 
-	/**
-	 * Constructs the object.
-	 */
-	Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        // empty constructor
+    }
 
-	}
-
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = Integer.parseInt(sc.nextLine());
-		String[] s = new String[n];
-		int w = 0;
-		LSD lsd = new LSD();
-		for (int i = 0; i < n; i++) {
-			s[i] = sc.nextLine();
-			w = s[i].length();
-		}
-		String[] a = lsd.sort(s, w);
-		System.out.println(Arrays.toString(a));
-	}
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        String[] s = new String[n];
+        int w = 0;
+        LSD lsd = new LSD();
+        for (int i = 0; i < n; i++) {
+            s[i] = sc.nextLine();
+            w = s[i].length();
+        }
+        String[] a = lsd.sort(s, w);
+        System.out.println(Arrays.toString(a));
+    }
 }
